361d033bfe04cfecfaed8c5ad7437a507772ede7
Paul Scherer <pscherer413@gmail.com>
2013-10-17 00:46:36 -0700

    main.cc: - Placed code into methods - Use variable yyin to read pipe from cpp
    Makefile:
    - Added code for more sources (asg 2 files)
    
    astree.cc:
    astree.h:
    auxlib.cc
    auxlib.h
    lyutils.cc
    lyutils.h
    oclib.oh:
    parser.y:
    scanner.l:
    - Initial commit

--- Makefile
+++ Makefile
@@ -1,43 +1,141 @@
-# Author: Paul Scherer, pscherer@ucsc.edu
+# Paul Scherer, pscherer@ucsc.edu
 
-GPP   = g++ -g -O0 -Wall -Wextra -std=gnu++0x
-GRIND = valgrind --leak-check=full --show-reachable=yes
+MKFILE    = Makefile
+DEPSFILE  = ${MKFILE}.deps
+NOINCLUDE = ci clean spotless
+NEEDINCL  = ${filter ${NOINCLUDE}, ${MAKECMDGOALS}}
+VALGRIND  = valgrind --leak-check=full --show-reachable=yes
 
-CHEADER = stringset.h
-CSOURCE = main.cc stringset.cc
-OBJECTS = ${CSOURCE:.cc=.o}
-EXECBIN = oc
-SOURCES = ${CHEADER} ${CSOURCE} Makefile README oclib.oh
-SUBMIT = submit cmps104a-wm.f13 asg1
+#
+# Definitions of list of files:
+#
+HSOURCES  = astree.h  lyutils.h  auxlib.h  stringset.h
+CSOURCES  = astree.cc lyutils.cc auxlib.cc stringset.cc main.cc
+LSOURCES  = scanner.l
+YSOURCES  = parser.y
+ETCSRC    = README ${MKFILE} ${DEPSFILE}
+CLGEN     = yylex.cc
+HYGEN     = yyparse.h
+CYGEN     = yyparse.cc
+CGENS     = ${CLGEN} ${CYGEN}
+ALLGENS   = ${HYGEN} ${CGENS}
+EXECBIN   = oc
+ALLCSRC   = ${CSOURCES} ${CGENS}
+OBJECTS   = ${ALLCSRC:.cc=.o}
+LREPORT   = yylex.output
+YREPORT   = yyparse.output
+IREPORT   = ident.output
+REPORTS   = ${LREPORT} ${YREPORT} ${IREPORT}
+ALLSRC    = ${ETCSRC} ${YSOURCES} ${LSOURCES} ${HSOURCES} ${CSOURCES}
+TESTINS   = ${wildcard test?.in}
+LISTSRC   = ${ALLSRC} ${HYGEN}
+SUBMIT = submit cmps104a-wm.f13 asg2
 
-all : oc
+#
+# Definitions of the compiler and compilation options:
+#
+GCC       = g++ -g -O0 -Wall -Wextra -std=gnu++0x
+MKDEPS    = g++ -MM -std=gnu++0x
 
-oc : main.o stringset.o
-	${GPP} main.o stringset.o -o oc
+#
+# The first target is always ``all'', and hence the default,
+# and builds the executable images
+#
+all : ${EXECBIN}
 
+#
+# Build the executable image from the object files.
+#
+${EXECBIN} : ${OBJECTS}
+	${GCC} -o${EXECBIN} ${OBJECTS}
+	ident ${OBJECTS} ${EXECBIN} >${IREPORT}
+
+#
+# Build an object file form a C source file.
+#
 %.o : %.cc
-	${GPP} -c $<
+	${GCC} -c $<
 
+
+#
+# Checksource files.
+#
 check :
 	- checksource ${SOURCES}
 
-spotless : clean
-	- rm -f cpp_output *.str test.out ${OBJECTS}
+#
+# Build the scanner.
+#
+${CLGEN} : ${LSOURCES}
+	flex --outfile=${CLGEN} ${LSOURCES} 2>${LREPORT}
+	- grep -v '^  ' ${LREPORT}
+	- (perl -e 'print "="x65,"\n"'; cat lex.backup) >>${LREPORT} 
+	- rm lex.backup
 
+#
+# Build the parser.
+#
+${CYGEN} ${HYGEN} : ${YSOURCES}
+	bison --defines=${HYGEN} --output=${CYGEN} ${YSOURCES}
+
+#
+# Check sources into an RCS subdirectory.
+#
+ci : ${ALLSRC} ${TESTINS}
+	cid + ${ALLSRC} ${TESTINS} test?.inh
+
+#
+# Make a listing from all of the sources
+#
+lis : ${LISTSRC} tests
+	mkpspdf List.source.ps ${LISTSRC}
+	mkpspdf List.output.ps ${REPORTS} \
+		${foreach test, ${TESTINS:.in=}, \
+		${patsubst %, ${test}.%, in out err}}
+
+#
+# Clean and spotless remove generated files.
+#
 clean :
-	- rm ${EXECBIN}
+	- rm ${OBJECTS} ${ALLGENS} ${REPORTS} ${DEPSFILE} core
+	- rm ${foreach test, ${TESTINS:.in=}, \
+		${patsubst %, ${test}.%, out err}}
 
-grind : oc
-	${GRIND} oc 01-hello.oc >test.out 2>&1
+spotless : clean
+	- rm ${EXECBIN} *.str List.*.ps List.*.pdf
 
-lis : test.out
-	mkpspdf Listing.ps stringset.h stringset.cc main.cc \
-	        Makefile test.out
+
+#
+# Build the dependencies file using the C preprocessor
+#
+deps : ${ALLCSRC}
+	@ echo "# ${DEPSFILE} created `date` by ${MAKE}" >${DEPSFILE}
+	${MKDEPS} ${ALLCSRC} >>${DEPSFILE}
 
-submit : ${SOURCES}
-	${SUBMIT} ${SOURCES}
+${DEPSFILE} :
+	@ touch ${DEPSFILE}
+	${MAKE} --no-print-directory deps
 
-# Depencencies.
-main.o: main.cc stringset.h
-stringset.o: stringset.cc stringset.h
+#
+# Test
+#
+
+tests : ${EXECBIN}
+	touch ${TESTINS}
+	make --no-print-directory ${TESTINS:.in=.out}
+
+%.out %.err : %.in ${EXECBIN}
+	( ${VALGRIND} ${EXECBIN} -ly -@@ $< \
+	;  echo EXIT STATUS $$? 1>&2 \
+	) 1>$*.out 2>$*.err
+
+#
+# Everything
+#
+again :
+	gmake --no-print-directory spotless deps ci all lis
+	
+ifeq "${NEEDINCL}" ""
+include ${DEPSFILE}
+endif
 
--- main.cc
+++ main.cc
@@ -9,13 +9,68 @@ using namespace std;
 #include <stdlib.h>
 #include <string.h>
 
+#include "lyutils.h"
 #include "stringset.h"
 
 const size_t LINESIZE = 1024;
+string at_value = "";
+string dvalue = "";
+
+const string CPP = "/usr/bin/cpp";
+
+void yyin_cpp_popen (string path, FILE *oc_file) {
+   // If the -D option was passed, pass option for cpp command, else
+   // call "cpp infile outfile".
+   string cpp_d_command = CPP + " -D " + dvalue + " " + path;
+   string cpp_command = CPP + " " + path;
+   if (dvalue.compare("") != 0)
+      yyin = popen (cpp_d_command.c_str(), "r");
+   else
+      yyin = popen (cpp_command.c_str(), "r");
+   fclose (oc_file);
+
+   // Check for a valid cpp output file and open it.
+   if (yyin == NULL) {
+      syserrprintf (cpp_command.c_str());
+      exit (get_exitstatus());
+   }
+}
+
+void scan_opts (int argc, char** argv) {
+   // Flags for option parameters passed.
+   int lflag = 0;
+   int yflag = 0;
+
+   // Activate flags if passed in command arguments.
+   int c;
+   while ((c = getopt (argc, argv, "@:D:ly")) != -1) {
+      switch (c) {
+      case '@':
+         at_value = optarg;
+         break;
+      case 'D':
+         dvalue = optarg;
+         break;
+      case 'l':
+         lflag = 1;
+         break;
+      case 'y':
+         yflag = 1;
+         break;
+      case '?':
+         if (optopt == '@' || optopt == 'D')
+            fprintf (stderr, "Option -%c requires an argument.\n",
+                  optopt);
+         else
+            fprintf (stderr, "Unknown option '-%c'.\n", optopt);
+      default:
+         set_exitstatus (EXIT_FAILURE);
+         exit (get_exitstatus());
+      }
+   }
+}
 
 int main (int argc, char **argv) {
-   const string CPP = "/usr/bin/cpp";
-   string cpp_filename = "cpp_output";
    string prog_name;          // Name of program passed
    size_t period_pos;         // Index of period for file extension
    string delim = "\\ \t\n";
@@ -28,13 +83,15 @@ int main (int argc, char **argv) {
    period_pos = path.find_last_of (".");
    if (period_pos == string::npos) {
       fprintf (stderr, "Must pass .oc file.\n");
-      exit (EXIT_FAILURE);
+      set_exitstatus (EXIT_FAILURE);
+      exit (get_exitstatus());
    }
 
    string file_type = path.substr(period_pos + 1);
    if (file_type.compare ("oc") != 0) {
       fprintf (stderr, "Invalid file type '.%s'.\n", file_type.c_str());
-      exit (EXIT_FAILURE);
+      set_exitstatus (EXIT_FAILURE);
+      exit (get_exitstatus());
    }
 
    // Get program name.
@@ -49,60 +106,13 @@ int main (int argc, char **argv) {
    if (oc_file == NULL) {
       fprintf (stderr, "Cannot access '%s': No such file or"
             " directory.\n", path.c_str());
-      exit (EXIT_FAILURE);
+      set_exitstatus (EXIT_FAILURE);
+      exit (get_exitstatus());
    }
 
-   // Flags for option parameters passed.
-   string at_value = "";
-   string dvalue = "";
-   int lflag = 0;
-   int yflag = 0;
+   scan_opts (argc, argv);
 
-   // Activate flags if passed in command arguments.
-   int c;
-   while ((c = getopt (argc, argv, "@:D:ly")) != -1) {
-      switch (c) {
-         case '@':
-            at_value = optarg;
-            break;
-         case 'D':
-            dvalue = optarg;
-            break;
-         case 'l':
-            lflag = 1;
-            break;
-         case 'y':
-            yflag = 1;
-            break;
-         case '?':
-            if (optopt == '@' || optopt == 'D')
-               fprintf (stderr, "Option -%c requires an argument.\n",
-                     optopt);
-            else
-               fprintf (stderr, "Unknown option '-%c'.\n", optopt);
-            return 1;
-         default:
-            exit (EXIT_FAILURE);
-      }
-   }
-
-   // If the -D option was passed, pass option for cpp command, else
-   // call "cpp infile outfile".
-   string cpp_d_command = CPP + " -D " + dvalue + " " + path;
-   string cpp_command = CPP + " " + path;
-   FILE *cpp_file;
-   if (dvalue.compare("") != 0)
-      cpp_file = popen (cpp_d_command.c_str(), "r");
-   else
-      cpp_file = popen (cpp_command.c_str(), "r");
-   fclose (oc_file);
-
-   // Check for a valid cpp output file and open it.
-   if (cpp_file == NULL) {
-         fprintf (stderr, "Cannot access '%s': No such file or"
-               " directory.\n", cpp_filename.c_str());
-         exit (EXIT_FAILURE);
-   }
+   yyin_cpp_popen (path, oc_file);
 
    // Create a program.str file
    FILE *str_file = fopen ((prog_name + ".str").c_str(), "w");
@@ -110,7 +120,7 @@ int main (int argc, char **argv) {
    // Read line of cpp output file and tokenize it, and insert it into
    // the string set.
    char buffer[LINESIZE];
-   while (fgets (buffer, LINESIZE, cpp_file) != NULL) {
+   while (fgets (buffer, LINESIZE, yyin) != NULL) {
       token = strtok (buffer, delim.c_str());
 
       while (token != NULL) {
@@ -123,9 +133,10 @@ int main (int argc, char **argv) {
    dump_stringset (str_file);
    fclose (str_file);
 
-   if (pclose (cpp_file)) {
-      return EXIT_FAILURE;
+   if (pclose (yyin)) {
+      set_exitstatus (EXIT_FAILURE);
+      exit (get_exitstatus());
    }
 
-   return EXIT_SUCCESS;
+   exit (get_exitstatus());
 }
